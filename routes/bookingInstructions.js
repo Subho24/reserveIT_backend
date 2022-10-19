a45f0fbf34b5207773995a0a033fda503738991f
Subhojit Saha <subhojitan@gmail.com>
2022-10-20 01:45:15 +0200

    Added custom query feature to booking_instruction get API

--- routes/bookingInstructions.js
+++ routes/bookingInstructions.js
@@ -13,7 +13,8 @@ router.get('/:company_id', async (req, res) => {
     const db = await dbHandler.createConnectionAsync();
 
     try {
-        const query = `Select * from booking_instructions Where company_id = ${req.params.company_id}`;
+        const query = req.query.custom ? `Select ${req.query.custom} from booking_instructions Where company_id = ${req.params.company_id}` 
+        : `Select * from booking_instructions Where company_id = ${req.params.company_id}`;
         console.log(await dbHandler.connectAsync(db))
         const data = await dbHandler.queryAsync(db, query);
         res.status(200).json(data);
